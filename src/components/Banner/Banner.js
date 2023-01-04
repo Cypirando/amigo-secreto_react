01560b51fb70b581a47b062c8c465e89b43089da
cypirando <fabiocypriano@gmail.com>
2023-01-04 18:11:31 -0300

    First commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/images/bot-logo.jpg" alt="O banner principal da página do Botdesigner"/>
+        </header>
+    )
+}
