188611f1e00b18f810931ebb5ca3422b87dc05eb
cypirando <fabiocypriano@gmail.com>
2023-01-18 15:35:13 -0300

    teste

--- src/api/index.js
+++ src/api/index.js
@@ -1,61 +1,23 @@
+
+import axios from "axios";
+
 const rota = "http://localhost:5000/nomes-sortados";
 
 async function criarSorteio(nomes) {
   try {
-    let header = new Headers({
-      "Content-Type": "application/json",
-    });
-    let request = await fetch(rota, {
-      method: "POST",
-      headers: header,
-      body: JSON.stringify({ nomes }),
-    });
-    request
-      .json()
-      .then((sorteados) => {})
-      .catch((err) => console.log("err=", err));
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    };
+    const request = await axios.post(rota, { nomes }, config);
+    const sorteados = request.data;
+    console.log(sorteados);
   } catch (error) {
-    console.log("eror");
+    console.log(error);
+    alert("There was an error with the API call. Please try again later.");
   }
 }
 
 export default criarSorteio;
-
-
-// import { useState } from "react";
-
-// const rota = "http://localhost:5000/nomes-sortados";
-// const CriarNovoSorteio = () => {
-//   const [erro, setErro] = useState(false);
-//   const [mensagemErro, setMensagemErro] = useState("");
-
-//   async function criarSorteio(nomes) {
-//     try {
-//       let header = new Headers({
-//         "Content-Type": "application/json",
-//       });
-//       let request = await fetch(rota, {
-//         method: "POST",
-//         headers: header,
-//         body: JSON.stringify({ nomes }),
-//       });
-//       request
-//         .json()
-//         .then((sorteados) => {})
-//         .catch((err) => console.log("err=", err));
-//     } catch (error) {
-//       setErro(true);
-//       setMensagemErro(
-//         "Ocorreu um erro ao criar o sorteio. Por favor, tente novamente mais tarde."
-//       );
-//     }
-//   }
-
-//   return (
-//     <div>
-//       {erro && <div className="mensagem-erro">{mensagemErro}</div>}
-//       <form onSubmit={criarSorteio}>...</form>
-//     </div>
-//   );
-// };
-// export default CriarNovoSorteio;
