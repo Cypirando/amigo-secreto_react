1812e73c512a1471335ae4623daead369335b2fd
cypirando <fabiocypriano@gmail.com>
2023-01-18 21:10:56 -0300

    bug na API

--- src/components/Participantes/index.jsx
+++ src/components/Participantes/index.jsx
@@ -6,30 +6,12 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import Erro from "../Erro";
 
-// import { v4 as uuidv4 } from "uuid";
-
 const Participantes = ({ aoDeletar, colaboradores }) => {
   const [erro, setError] = useState();
   const location = useLocation();
   let navigate = useNavigate();
 
   console.log(colaboradores);
-  
-  // async function onClick() {
-  //   try {
-  //     await criarSorteio(colaboradores);
-  //     navigate("/fim");
-  //   } catch (error) {
-  //     console.log("erro");
-  //     if (error.response) {
-  //       console.log(error.response.data);
-  //       console.log(error.response.statusCode);
-  //     setError(400)
-  //     }else{
-  //       console.log(erro)
-  //     }
-  //   }
-  // }
 
   async function onClick() {
     try {
@@ -39,26 +21,32 @@ const Participantes = ({ aoDeletar, colaboradores }) => {
       if (error.response) {
         switch (error.response.status) {
           case 400:
-            console.log("Bad request");
+            setError(
+              "Bad request: Por favor, verifique se todos os campos estão preenchidos corretamente."
+            );
             break;
           case 401:
-            console.log("Unauthorized");
+            setError(
+              "Unauthorized: Não autorizado, verifique suas credenciais."
+            );
             break;
           case 500:
-            console.log("Internal Server Error");
+            setError(
+              "Internal Server Error: Ocorreu um erro no servidor, por favor, tente novamente mais tarde."
+            );
             break;
           default:
-            console.log(`Error: ${error.response.status}`);
+            setError(`Erro: ${error.response.status}`);
             break;
         }
-      }
-      if (error.response) {
-        setError(error.response.data);
       } else {
-        setError("Ocorreu um erro com a chamada de API. Por favor, tente novamente mais tarde.");
+        setError(
+          "Ocorreu um erro com a chamada de API. Por favor, tente novamente mais tarde."
+        );
       }
     }
   }
+
   return (
     <div className="participantes">
       {erro && <Erro message={erro} />}
